83633646af3b974a94a8acd1d08d2e17be8ecfae
Jay Wen <jaywen89@gmail.com>
2020-03-21 15:51:23 -0400

    fixes container and components

--- air-control/src/containers/MainContainer.js
+++ air-control/src/containers/MainContainer.js
@@ -3,27 +3,28 @@ import TreeContainer from "./TreeContainer";
 import FirewoodContainer from "./FirewoodContainer";
 import StatsContainer from "./StatsContainer";
 
-class MainContainer extends React.Component() {
+class MainContainer extends React.Component {
+  state = { user: {} };
 
-  state = { trees: []}
+  componentDidMount() {
+    fetch("http://localhost:3000/users/1")
+      .then(response => response.json())
+      .then(user => this.setState({ user: user, trees: [1, 2, 3] }));
+  }
 
-//   componentDidMount() {
-//   fetch(`http://localhost:3000/users/${id}`)
-//   .then(response => response.json())
-//   .then(trees => this.setState({ trees: trees }));
-// }
-
-render(){
-  return (
-    <div className="main-container">
-      <h1>Main container</h1>
-      <StatsContainer />
-      <TreeContainer />
-      <FirewoodContainer />
-    </div>
-  );
-
-}
+  render() {
+    console.log(this.state.user);
+    console.log(this.state.user.trees);
+    return (
+      <div className="main-container">
+        <h1>Main container</h1>
+        <StatsContainer />
+        <TreeContainer />
+        <FirewoodContainer />
+      </div>
+    );
+  }
+  
 }
 
 export default MainContainer;
