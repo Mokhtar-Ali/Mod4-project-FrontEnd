30949f730887711fb11929d87c0bae0a5a897378
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-03-23 10:27:05 -0400

    new Branch, working on routing after signup

--- air-control/src/App.js
+++ air-control/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import MainContainer from "./containers/MainContainer";
 import NavBar from "./containers/Navbar";
