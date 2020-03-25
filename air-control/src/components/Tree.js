dacff1d90a1303d0e8010061d12c68914d0d358b
Jay Wen <jaywen89@gmail.com>
2020-03-25 13:22:52 -0400

    adds tree image

--- air-control/src/components/Tree.js
+++ air-control/src/components/Tree.js
@@ -1,11 +1,12 @@
 import React from 'react'
 // import adult-pine-tree from 'public/game-icons/adult-pine-tree.jpeg'
 
+// import tree from '../'l
 
 function Tree() {
   return (
     <div className="tree">
-      <h1>Tree</h1>
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fimages_k%2Ftransparent-pine-tree%2Ftransparent-pine-tree-2.jpg"></img>
     </div>
   );
 }
