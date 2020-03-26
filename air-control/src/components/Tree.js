e8d562e84adb61bf237e14932d6622ab993f9ce1
Jay Wen <jaywen89@gmail.com>
2020-03-26 13:43:54 -0400

    workign on styling

--- air-control/src/components/Tree.js
+++ air-control/src/components/Tree.js
@@ -4,6 +4,7 @@ import React from 'react'
 function Tree(props) {
   return (
     <div className="tree">
+      <p>ind tree</p>
       {/* <img src={props.atmosphere.tree.image}/> */}
     </div>
   )
